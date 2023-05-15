56ee0dddc22f4b60604709fecd6f6ccf2e510505
Kunal Mukherjee <kunalm.8470@outlook.com>
2023-05-15 23:29:48 +0530

    Added in memory caching

---
+++ 4. Caching/inmemory-caching/src/Api/Controllers/PersonsController.cs
@@ -0,0 +1,49 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PersonsController : ControllerBase
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public PersonsController(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Person>>> GetPeopleAsync()
+    {
+        if (!_memoryCache.TryGetValue("people", out IReadOnlyList<Person> people))
+        {
+            people = Enumerable.Range(1, 100)
+            .Select(_ =>
+            {
+                Bogus.Faker faker = new("en");
+
+                return new Person
+                {
+                    FirstName = faker.Person.FirstName,
+                    LastName = faker.Person.LastName,
+                    Email = faker.Person.Email,
+                    Gender = faker.Person.Gender.ToString()
+                };
+            })
+            .ToList()
+            .AsReadOnly();
+
+            await Task.Delay(2000);
+
+            MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+            _memoryCache.Set("people", people, cacheEntryOptions);
+        }
+
+        return Ok(people);
+    }
+}
